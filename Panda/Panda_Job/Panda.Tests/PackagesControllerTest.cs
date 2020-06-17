44bf89ec0b41c1cd488a089f9daeeb56c694c136
Radoslav Hadzhiiliev <radoslavcfc05@gmail.com>
2020-06-17 15:55:23 +0300

    Rearranging code structure

--- Panda/Panda_Job/Panda.Tests/PackagesControllerTest.cs
+++ Panda/Panda_Job/Panda.Tests/PackagesControllerTest.cs
@@ -8,13 +8,10 @@ using Microsoft.Extensions.Options;
 using Moq;
 using Panda.App.Controllers;
 using Panda.Domain;
-using Panda.Domain.Enums;
 using Panda.Services;
 using PandaWeb.Models.Package;
 using System.Collections.Generic;
 using System.Linq;
-using System.Security.Claims;
-using System.Security.Principal;
 using System.Threading.Tasks;
 using Xunit;
 
@@ -22,7 +19,7 @@ namespace Panda.Tests
 {
     public class PackagesControllerTest
     {
-        public PackagesController InitializeControllerContstuctor()
+        private PackagesController InitializeControllerContstuctor()
         {
             var packageServiceMock = new Mock<IPackagesService>();
             var userManagerServiceMock = TestUserManager<PandaUser>();
@@ -32,79 +29,18 @@ namespace Panda.Tests
             var controller = new PackagesController
                (userManagerServiceMock, userServiceMock.Object, packageServiceMock.Object, addressServiceMock.Object);
 
-            controller.ControllerContext = this.InitializeHttpContextWithAdmin();
+            controller.ControllerContext = this.InitializeHttpContextWithRole("admin");
             return controller;
         }
 
-        private ControllerContext InitializeHttpContextWithAdmin()
+        private ControllerContext InitializeHttpContextWithRole(string role)
         {
             var httpContext = new Mock<HttpContext>();
-            httpContext.Setup(m => m.User.IsInRole("admin")).Returns(true);
+            httpContext.Setup(m => m.User.IsInRole(role)).Returns(true);
 
             var context = new ControllerContext(
                 new ActionContext(httpContext.Object, new RouteData(), new ControllerActionDescriptor()));
             return context;
-        }
-
-        [Fact]
-        public void PackagesController_IndexTest()
-        {
-            var controller = this.InitializeControllerContstuctor();
-
-            var httpContext = new Mock<HttpContext>();
-            httpContext.Setup(m => m.User.IsInRole("admin")).Returns(true);
-           
-            var context = new ControllerContext(
-                new ActionContext(httpContext.Object, new RouteData(), new ControllerActionDescriptor()));
-
-            controller.ControllerContext = context;
-           
-            var result = controller.Index();
-            Assert.IsType<ViewResult>(result);
-
-            //httpContext.Setup(m => m.User.IsInRole("user")).Returns(true);
-            //var resultWithUserRole = controller.Index();
-            //Assert.IsType<ViewResult>(resultWithUserRole);
-
-        }
-
-        [Fact]
-        public void PackagesController_IndexTest_CountOfPackages()
-        {
-            var mockPakcageService = new Mock<IPackagesService>();
-            var userManagerServiceMock = TestUserManager<PandaUser>();
-            var addressServiceMock = new Mock<IAddressesService>();
-            var userServiceMock = new Mock<IUsersService>();
-
-            var controller = new PackagesController
-               (userManagerServiceMock, userServiceMock.Object, mockPakcageService.Object, addressServiceMock.Object);
-
-            mockPakcageService.Setup(list => list.GetAllPackages())
-                .Returns(this.GetSampleListPackage());
-
-            var httpContext = new Mock<HttpContext>();
-            httpContext.Setup(m => m.User.IsInRole("admin")).Returns(true);
-            var context = new ControllerContext(
-                new ActionContext(httpContext.Object, new RouteData(), new ControllerActionDescriptor()));
-            controller.ControllerContext = context;
-
-            var result = controller.Index();
-
-            var viewResult = Assert.IsType<ViewResult>(result);
-
-            //Controller transforming the result from the service to List<PackageHomeViewModel>(), after mapping it
-            var model = Assert.IsAssignableFrom<List<PackageHomeViewModel>> (
-            viewResult.ViewData.Model);
-            Assert.Equal(2, model.Count());
-        }
-
-            [Fact]
-        public void PackagesController_CreateTest()
-        {
-            var controller = this.InitializeControllerContstuctor();
-
-            var result = controller.Create();
-            Assert.IsType<ViewResult>(result);
         }
 
         private IQueryable<Package> GetSampleListPackage()
@@ -115,7 +51,7 @@ namespace Panda.Tests
 
             return list.AsQueryable<Package>();
         }
-        private static UserManager<TUser> TestUserManager<TUser>(IUserStore<TUser> store = null) 
+        private static UserManager<TUser> TestUserManager<TUser>(IUserStore<TUser> store = null)
             where TUser : class
         {
             store = store ?? new Mock<IUserStore<TUser>>().Object;
@@ -136,5 +72,59 @@ namespace Panda.Tests
                 .Returns(Task.FromResult(IdentityResult.Success)).Verifiable();
             return userManager;
         }
+
+        [Fact]
+        public void PackagesController_IndexTest()
+        {
+            var controller = this.InitializeControllerContstuctor();
+
+            var context = this.InitializeHttpContextWithRole("admin");
+            controller.ControllerContext = context;
+           
+            var result = controller.Index();
+            Assert.IsType<ViewResult>(result);
+
+            context = this.InitializeHttpContextWithRole("user");
+            var resultWithUserRole = controller.Index();
+            Assert.IsType<ViewResult>(resultWithUserRole);
+
+        }
+
+        [Fact]
+        public void PackagesController_IndexTest_CountOfPackages()
+        {
+            var mockPakcageService = new Mock<IPackagesService>();
+            var userManagerServiceMock = TestUserManager<PandaUser>();
+            var addressServiceMock = new Mock<IAddressesService>();
+            var userServiceMock = new Mock<IUsersService>();
+
+            var controller = new PackagesController
+               (userManagerServiceMock, userServiceMock.Object, mockPakcageService.Object, addressServiceMock.Object);
+
+            mockPakcageService.Setup(list => list.GetAllPackages())
+                .Returns(this.GetSampleListPackage());
+
+            controller.ControllerContext = this.InitializeHttpContextWithRole("admin");
+
+            var result = controller.Index();
+
+            var viewResult = Assert.IsType<ViewResult>(result);
+
+            //Controller transforming the result from the service to List<PackageHomeViewModel>(), after mapping it
+            var model = Assert.IsAssignableFrom<List<PackageHomeViewModel>> (
+            viewResult.ViewData.Model);
+            Assert.Equal(2, model.Count());
+        }
+
+            [Fact]
+        public void PackagesController_CreateTest()
+        {
+            var controller = this.InitializeControllerContstuctor();
+
+            var result = controller.Create();
+            Assert.IsType<ViewResult>(result);
+        }
+
+        
     }
 }
