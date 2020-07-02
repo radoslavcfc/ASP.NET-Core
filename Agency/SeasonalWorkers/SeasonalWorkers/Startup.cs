4b6c43397dbfd19243f75df14d73d9204094d6ca
Radoslav Hadzhiiliev <radoslavcfc05@gmail.com>
2020-07-02 18:55:14 +0300

    Data class library

--- Agency/SeasonalWorkers/SeasonalWorkers/Startup.cs
+++ Agency/SeasonalWorkers/SeasonalWorkers/Startup.cs
@@ -1,18 +1,12 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 using Microsoft.AspNetCore.Builder;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Identity.UI;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
+using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Identity;
 using Microsoft.EntityFrameworkCore;
-using SeasonalWorkers.Data;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.AspNetCore.Http;
+using SeasonalWorkers.Data;
 
 namespace SeasonalWorkers
 {
