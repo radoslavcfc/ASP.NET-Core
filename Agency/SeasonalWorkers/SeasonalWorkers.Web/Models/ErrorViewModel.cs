d93fc08bd9159b93348291041a515e4769e3b256
Radoslav Hadzhiiliev <radoslavcfc05@gmail.com>
2020-07-05 18:59:46 +0300

    Update

---
+++ Agency/SeasonalWorkers/SeasonalWorkers.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace SeasonalWorkers.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
