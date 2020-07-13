df3f3a54e23712224d61d4075895a45f240e3bc6
Radoslav Hadzhiiliev <radoslavcfc05@gmail.com>
2020-07-13 22:05:15 +0300

    Update

--- Agency/SeasonalWorkers/SeasonalWorkers.Web/Startup.cs
+++ Agency/SeasonalWorkers/SeasonalWorkers.Web/Startup.cs
@@ -1,7 +1,7 @@
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
-using Microsoft.AspNetCore.Identity;
+
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
