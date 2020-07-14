f0e8a7598b76a1cd89c4213d7ef54e19168f3f8f
Radoslav Hadzhiiliev <radoslavcfc05@gmail.com>
2020-07-14 17:18:12 +0300

    custom user

--- Agency/Agency.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
+++ Agency/Agency.Web/Areas/Identity/Pages/Account/Login.cshtml.cs
@@ -11,19 +11,20 @@ using Microsoft.AspNetCore.Identity.UI.Services;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.AspNetCore.Mvc.RazorPages;
 using Microsoft.Extensions.Logging;
+using Agency.Data.Models;
 
 namespace Agency.Web.Areas.Identity.Pages.Account
 {
     [AllowAnonymous]
     public class LoginModel : PageModel
     {
-        private readonly UserManager<IdentityUser> _userManager;
-        private readonly SignInManager<IdentityUser> _signInManager;
+        private readonly UserManager<AgencyUser> _userManager;
+        private readonly SignInManager<AgencyUser> _signInManager;
         private readonly ILogger<LoginModel> _logger;
 
-        public LoginModel(SignInManager<IdentityUser> signInManager, 
+        public LoginModel(SignInManager<AgencyUser> signInManager, 
             ILogger<LoginModel> logger,
-            UserManager<IdentityUser> userManager)
+            UserManager<AgencyUser> userManager)
         {
             _userManager = userManager;
             _signInManager = signInManager;
