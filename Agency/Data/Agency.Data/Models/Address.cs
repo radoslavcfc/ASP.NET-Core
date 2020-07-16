c18c02d2bd0f8dec0ade2f0985fafad9dd12bb9f
Radoslav Hadzhiiliev <radoslavcfc05@gmail.com>
2020-07-16 16:10:02 +0300

    Update Address.cs

--- Agency/Data/Agency.Data/Models/Address.cs
+++ Agency/Data/Agency.Data/Models/Address.cs
@@ -4,5 +4,14 @@ namespace Agency.Data.Models
 {
     public class Address : BaseDeletableModel<string>
     {
+        public string Number { get; set; }
+        public string Street { get; set; }
+        public string Town { get; set; }
+        public string Region { get; set; }
+        public string Couttry { get; set; }
+        public int PostCode { get; set; }
+        public int Flat { get; set; }
+        public int Floor { get; set; }
+        public string Entrance { get; set; }
     }
 }
