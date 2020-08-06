c3e3f33a237cd1a6dba450b358af6122f9951ee2
Radoslav Hadzhiiliev <radoslavcfc05@gmail.com>
2020-08-06 15:03:47 +0300

    Work On Creating Names

--- Agency/Agency.Web/Controllers/NamesController.cs
+++ Agency/Agency.Web/Controllers/NamesController.cs
@@ -1,10 +1,23 @@
-﻿using Microsoft.AspNetCore.Mvc;
-
+﻿using Agency.Services;
+using Agency.Web.Models.Names;
+using Microsoft.AspNetCore.Mvc;
+using System.Threading.Tasks;
 
 namespace Agency.Web.Controllers
 {
     public class NamesController : Controller
     {
+        private readonly INamesService namesService;
 
+        public NamesController(INamesService namesService)
+        {
+            this.namesService = namesService;
+        }
+
+        public async Task<IActionResult> Create()
+        {
+            var createNamesModel = new CreateNamesModel();
+            return this.View();
+        }
     }
 }
---
+++ Agency/Agency.Web/Models/Names/CreateNamesModel.cs
@@ -0,0 +1,10 @@
+﻿namespace Agency.Web.Models.Names
+{
+    public class CreateNamesModel
+    {
+        public string FirstName { get; set; }
+        public string MiddleName { get; set; }
+
+        public string LastName { get; set; }
+    }
+}
