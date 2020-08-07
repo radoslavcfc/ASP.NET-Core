d6c56efab1e9a8146955f8b96516eb0ba6e58fbc
Radoslav Hadzhiiliev <radoslavcfc05@gmail.com>
2020-08-07 14:55:48 +0300

    relation updated

--- Agency/Agency.Web/Migrations/20200807115514_relation.Designer.cs
+++ Agency/Agency.Web/Migrations/20200807115514_relation.Designer.cs
@@ -4,14 +4,16 @@ using Agency.Web.Data;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Infrastructure;
 using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
 using Microsoft.EntityFrameworkCore.Storage.ValueConversion;
 
 namespace Agency.Web.Migrations
 {
     [DbContext(typeof(AgencyDbContext))]
-    partial class AgencyDbContextModelSnapshot : ModelSnapshot
+    [Migration("20200807115514_relation")]
+    partial class relation
     {
-        protected override void BuildModel(ModelBuilder modelBuilder)
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
         {
 #pragma warning disable 612, 618
             modelBuilder
---
+++ Agency/Agency.Web/Migrations/20200807115514_relation.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Agency.Web.Migrations
+{
+    public partial class relation : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_WorkerId",
+                table: "AspNetUsers");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_WorkerId",
+                table: "AspNetUsers",
+                column: "WorkerId",
+                unique: true,
+                filter: "[WorkerId] IS NOT NULL");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropIndex(
+                name: "IX_AspNetUsers_WorkerId",
+                table: "AspNetUsers");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_AspNetUsers_WorkerId",
+                table: "AspNetUsers",
+                column: "WorkerId");
+        }
+    }
+}
