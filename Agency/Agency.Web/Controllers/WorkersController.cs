f1f8eea85ec4b330f251c65cccd79b2d191b86ab
Radoslav Hadzhiiliev <radoslavcfc05@gmail.com>
2020-08-10 11:00:39 +0300

    Complete Worker data process

---
+++ Agency/Agency.Web/Controllers/WorkersController.cs
@@ -0,0 +1,23 @@
+﻿using Agency.Data.Models;
+using Agency.Services;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.Extensions.Logging;
+
+namespace Agency.Web.Controllers
+{
+    public class WorkersController : Controller
+    {
+        private readonly ILogger<NamesController> _logger;
+        private readonly IWorkersService _workersService;
+        private readonly UserManager<AgencyUser> _userManager;
+
+        public WorkersController(ILogger<NamesController> logger,
+                        IWorkersService workersService, UserManager<AgencyUser> userManager)
+        {
+            this._logger = logger;
+            this._workersService = workersService;
+            this._userManager = userManager;
+        }
+    }
+}
---
+++ Agency/Agency.Web/Models/Workers/CompleteWorkerDataModel.cs
@@ -0,0 +1,16 @@
+﻿using Agency.Data.Enums;
+using System;
+
+namespace Agency.Web.Models.Workers
+{
+    public class CompleteWorkerDataModel
+    {
+        public DateTime DOB { get; set; }
+        public Gender Gender { get; set; }
+        public string Relatives { get; set; }
+        public DateTime AvailableFrom { get; set; }
+        public DateTime AvailableTo { get; set; }
+        public string CurrentlyIn { get; set; }
+        public WorkerType WorkerType { get; set; }
+    }
+}
