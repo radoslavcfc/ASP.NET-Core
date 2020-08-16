ad8d524711fe4de6fd41fb782e22ff53912603c6
Radoslav Hadzhiiliev <radoslavcfc05@gmail.com>
2020-08-16 21:37:24 +0300

    Create NationalitiesSeeder.cs

---
+++ Agency/Data/Agency.Data/Seeding/NationalitiesSeeder.cs
@@ -0,0 +1,33 @@
+﻿using Agency.Data.Models;
+using Agency.Web.Data;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Agency.Data.Seeding
+{
+    public class NationalitiesSeeder : ISeeder
+    {
+        public async Task SeedAsync(AgencyDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext.Nationalities.Any())
+            {
+                return;
+            }
+
+            var nationalities = new List<string>
+            {
+               "British","Bulgarian","Romanian"
+            };
+
+            foreach (var nationality in nationalities)
+            {
+                await dbContext.Nationalities.AddAsync(new Nationality
+                {
+                    NationalityCountry = nationality
+                }) ;
+            }
+        }
+    }
+}
