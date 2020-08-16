343d93bab2d875f4273a5455e579277ffcfe85db
Radoslav Hadzhiiliev <radoslavcfc05@gmail.com>
2020-08-16 20:44:35 +0300

    Create ISeeder.cs

---
+++ Agency/Data/Agency.Data/Seeding/ISeeder.cs
@@ -0,0 +1,11 @@
+﻿using Agency.Web.Data;
+using System;
+using System.Threading.Tasks;
+
+namespace Agency.Data.Seeding
+{
+    public interface ISeeder
+    {
+        Task SeedAsync(AgencyDbContext dbContext, IServiceProvider serviceProvider);
+    }
+}
