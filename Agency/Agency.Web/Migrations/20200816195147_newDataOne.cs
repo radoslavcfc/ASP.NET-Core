bc5a052803aaf94115bdeeacdfacde92831fa802
Radoslav Hadzhiiliev <radoslavcfc05@gmail.com>
2020-08-16 22:57:47 +0300

    update db

---
+++ Agency/Agency.Web/Migrations/20200816195147_newDataOne.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Agency.Web.Migrations
+{
+    public partial class newDataOne : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+
+        }
+    }
+}
