d2077a9c395559aaf61b2d4e685adc79809097bd
Radoslav Hadzhiiliev <radoslavcfc05@gmail.com>
2020-08-16 21:37:18 +0300

    Update AgencyDbContextSeeder.cs

--- Agency/Data/Agency.Data/Seeding/AgencyDbContextSeeder.cs
+++ Agency/Data/Agency.Data/Seeding/AgencyDbContextSeeder.cs
@@ -1,10 +1,39 @@
-﻿using System;
+﻿using Agency.Web.Data;
+using Microsoft.Extensions.Logging;
+using System;
 using System.Collections.Generic;
 using System.Text;
+using System.Threading.Tasks;
 
 namespace Agency.Data.Seeding
 {
-    class AgencyDbContextSeeder
+    public class AgencyDbContextSeeder : ISeeder
     {
+        public async Task SeedAsync(AgencyDbContext dbContext, IServiceProvider serviceProvider)
+        {
+            if (dbContext == null)
+            {
+                throw new ArgumentNullException(nameof(dbContext));
+            }
+
+            if (serviceProvider == null)
+            {
+                throw new ArgumentNullException(nameof(serviceProvider));
+            }
+
+           
+            var seeders = new List<ISeeder>
+                          {
+                              //new RolesSeeder(),
+                              
+                          };
+
+            foreach (var seeder in seeders)
+            {
+                await seeder.SeedAsync(dbContext, serviceProvider);
+                await dbContext.SaveChangesAsync();
+               // logger.LogInformation($"Seeder {seeder.GetType().Name} done.");
+            }
+        }
     }
 }
